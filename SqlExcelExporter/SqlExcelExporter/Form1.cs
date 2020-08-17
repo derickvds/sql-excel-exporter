e501febeba5edff4fd47e4234676d06a5c39ffbf
Derick van der Sandt <derickvds@gmail.com>
2020-08-17 23:01:41 +0200

    Added data read and excel export

--- SqlExcelExporter/SqlExcelExporter/Form1.cs
+++ SqlExcelExporter/SqlExcelExporter/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
 
 namespace SqlExcelExporter
 {
@@ -19,9 +24,43 @@ namespace SqlExcelExporter
 
         private void ExportBtn_Click(object sender, EventArgs e)
         {
+
+            _Application excel = new _Excel.Application();
+            string cs = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            StringBuilder sb = new StringBuilder();
             Crypto crypto = new Crypto();
-            var name = crypto.DecryptCode("UDsM886D8EjbjmI19DyO7g==");
-            MessageBox.Show(name);
+            Workbook wb;
+            Worksheet ws;
+
+            wb = excel.Workbooks.Add();
+            ws = excel.Sheets["Sheet1"];
+            int row = 1;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT [PatientNumber],[FirstName],[LastName],[ContactNumber],[Address] FROM [Patients]", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                ds.Tables[0].TableName = "Patients";
+
+                foreach (DataRow patientRow in ds.Tables["Patients"].Rows)
+                {
+                    int patientNumber = Convert.ToInt32(patientRow["PatientNumber"]);
+                    ws.Cells[row, 1].Value2 = patientNumber.ToString();
+                    ws.Cells[row, 2].Value2 = crypto.DecryptCode(patientRow["FirstName"].ToString());
+                    ws.Cells[row, 3].Value2 = crypto.DecryptCode(patientRow["LastName"].ToString());
+                    ws.Cells[row, 4].Value2 = crypto.DecryptCode(patientRow["ContactNumber"].ToString());
+                    ws.Cells[row, 5].Value2 = crypto.DecryptCode(patientRow["Address"].ToString());
+
+                    row++;
+                }
+            }
+
+            
+            wb.SaveAs(@"C:\ExportedData\PatientsList.xls");
+            wb.Close();
+            MessageBox.Show("Done");
         }
     }
 }
